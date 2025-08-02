29e057147d54d2ed4d00f1a27448a0c80d877382
qkitzero <qkitzero@gmail.com>
2025-08-02 19:03:43 +0900

    fix: git

--- src/ant_pr.py
+++ src/ant_pr.py
@@ -25,6 +25,9 @@ RULES = load_config().get("rules", {})
 def get_changed_files():
     base = os.environ["BASE_SHA"]
     head = os.environ["HEAD_SHA"]
+    subprocess.run(["git", "config", "--global", "--add", "safe.directory", "/github/workspace"])
+    subprocess.run(["git", "fetch", "origin", base])
+    subprocess.run(["git", "fetch", "origin", head])
     cmd = ["git", "diff", "--numstat", f"{base}...{head}"]
     output = subprocess.check_output(cmd).decode().splitlines()
     return output
